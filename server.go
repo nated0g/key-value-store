baf219911bbef81775570923140eda7e340af3ae
nated0g <n.usher87@gmail.com>
2022-04-10 16:28:04 -0700

    Basic in memory store completed Closes #4.

--- server.go
+++ server.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -10,20 +12,77 @@ import (
 	"github.com/go-chi/chi/middleware"
 )
 
+var data = map[string]string{}
+
 func main() {
+	//**************************
+	// SETUP
+	//**************************
 	port := "8080"
 	if fromEnv := os.Getenv("PORT"); fromEnv != "" {
 		port = fromEnv
 	}
-	log.Printf("Starting up on http://localhost:%s", port)
 
 	r := chi.NewRouter()
 
+	//**************************
+	// MIDDLEWARE
+	//**************************
 	r.Use(middleware.Logger)
 
+	//**************************
+	// ROUTES
+	//**************************
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		JSON(w, map[string]string{"hello": "world"})
 	})
+
+	r.Get("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		data, err := Get(r.Context(), key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		w.Write([]byte(data))
+	})
+	r.Delete("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+
+		err := Delete(r.Context(), key)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		JSON(w, map[string]string{"status": "success"})
+	})
+
+	r.Post("/key/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := chi.URLParam(r, "key")
+		body, err := io.ReadAll(r.Body)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		err = Set(r.Context(), key, string(body))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			JSON(w, map[string]string{"error": err.Error()})
+			return
+		}
+
+		JSON(w, map[string]string{"status": "success"})
+	})
+
+	log.Printf("Starting up on http://localhost:%s", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
@@ -37,4 +96,18 @@ func JSON(w http.ResponseWriter, data interface{}) {
 	}
 
 	w.Write(b)
+}
+
+func Set(ctx context.Context, key, value string) error {
+	data[key] = value
+	return nil
+}
+
+func Get(ctx context.Context, key string) (string, error) {
+	return data[key], nil
+}
+
+func Delete(ctx context.Context, key string) error {
+	delete(data, key)
+	return nil
 }
