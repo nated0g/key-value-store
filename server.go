963841abe7d777d15f77a043f853dfd8e9ceb5af
nated0g <n.usher87@gmail.com>
2022-04-10 01:10:36 -0700

    Closes #3

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func main() {
@@ -16,8 +18,23 @@ func main() {
 	log.Printf("Starting up on http://localhost:%s", port)
 
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world."))
+		JSON(w, map[string]string{"hello": "world"})
 	})
 	log.Fatal(http.ListenAndServe(":"+port, r))
+}
+
+func JSON(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	b, err := json.Marshal(data)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		JSON(w, map[string]string{"error": err.Error()})
+		return
+	}
+
+	w.Write(b)
 }
